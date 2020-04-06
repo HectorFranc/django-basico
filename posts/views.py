2a4ab660be2c4fab6bd2b6d48b40d0da6e65336c
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-05 20:47:55 -0500

    Template system

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,36 @@
 # Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Utilities
 from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont blac',
-        'user': 'Yesica cortez',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036'
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Lactea',
-        'user': 'C. Vander',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076'
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
@@ -29,11 +38,4 @@ posts = [
 # Create your views here.
 def list_posts(request):
     """List existing posts"""
-    content = []
-    for post in posts:
-        content.append(f"""
-            <p><strong>{post['name']}</strong></p>
-            <p><small>{post['user']} - <i>{post['timestamp']}</i></small></p>
-            <figure><img src="{post['picture']}" /></figure>
-        """)
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
