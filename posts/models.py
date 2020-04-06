b50d3f2980f0e28251f89d6200ac63502fc53f94
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-05 22:20:55 -0500

    User model is_admin field added

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,14 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     """User model."""
+
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
 
     bio = models.TextField(blank=True)
 
@@ -17,3 +20,6 @@ class User(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
