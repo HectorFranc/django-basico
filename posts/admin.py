0fbbc90baaeb9c99c9533833868e27e1ea036d20
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-06 14:26:09 -0500

    Challenge: Register Post model on admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,23 @@
+# Django
 from django.contrib import admin
 
+# Models
+from posts.models import Post
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+    fieldsets = (
+        ('Post', {
+            'fields': ('title', 'photo')
+        }),
+        ('User', {
+            'fields': ('user', 'profile')
+        }),
+        ('Metadata', {
+            'fields': ('created', 'updated')
+        })
+    )
+
+    readonly_fields = ('created', 'updated')
