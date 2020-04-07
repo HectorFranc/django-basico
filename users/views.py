7e4675b031a1a3265660f1a12bcdc4d779061a67
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-06 20:57:49 -0500

    Django forms basics. Form class, validation, cleaned_data and errors.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 
 
@@ -10,6 +10,9 @@ from django.db.utils import IntegrityError
 # Models
 from django.contrib.auth.models import User
 from users.models import Profile
+
+# Forms
+from users.forms import ProfileForm
 
 
 # Create your views here.
@@ -66,4 +69,28 @@ def signup_view(request):
 
 
 def update_profile(request):
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+
+    return render(
+        request,
+        'users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form,
+        }
+    )
