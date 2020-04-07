e2e576ef1dab353976660507c24809fcbc482756
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-07 14:36:32 -0500

    PostForm creation and added its url

--- posts/views.py
+++ posts/views.py
@@ -15,3 +15,23 @@ def list_posts(request):
     """List existing posts"""
     posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts': posts})
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+
+    return render(
+        request=request,
+        template_name='posts/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile,
+        }
+    )
