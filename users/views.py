5e1cb181390d50dfe4d09c0189d3016439ab2272
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-04-07 14:09:02 -0500

    Show forms in django template

--- users/views.py
+++ users/views.py
@@ -68,6 +68,7 @@ def signup_view(request):
     return render(request, 'users/signup.html')
 
 
+@login_required
 def update_profile(request):
     profile = request.user.profile
     if request.method == 'POST':
